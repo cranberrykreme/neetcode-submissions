1c3f43cbb1d67634e1996433d8c5afd86b01ae89
Chris Purkiss <chris.purkiss99@gmail.com>
2026-05-26 19:39:56 +1000

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        shortest_window = ""
+        left_wall = 0
+        target_count = {}
+        curr_count = {}
+        curr_hits = 0
+
+        for c in t:
+            target_count[c] = target_count.get(c, 0) + 1
+
+        short_left, short_right = 0, 0
+        for i in range(len(s)):
+            curr = s[i]
+            target_count_of_curr = target_count.get(curr, 0)
+            curr_count_of_curr = curr_count.get(curr, 0)
+            if target_count_of_curr > 0:
+                curr_count_of_curr += 1
+                if curr_count_of_curr <= target_count_of_curr:
+                    curr_hits += 1
+                curr_count[curr] = curr_count_of_curr
+            while curr_hits == len(t):
+                if short_left-short_right == 0 or short_right-short_left > i-left_wall:
+                    short_left, short_right = left_wall, i+1
+                left_char_target_count = target_count.get(s[left_wall], 0)
+                left_char_curr_count = curr_count.get(s[left_wall], 0)
+                if 0 < left_char_target_count:
+                    if left_char_target_count >= left_char_curr_count:
+                        curr_hits -= 1
+                    curr_count[s[left_wall]] = left_char_curr_count - 1
+                left_wall += 1
+
+        return "" if short_left - short_right == 0 else s[short_left:short_right]
